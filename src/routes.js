9797a26eee3586d86f2ace34c2d07564c08251a2
andre110 <andre1@rede.ulbra.br>
2019-10-07 20:53:40 -0300

    Update

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ import Autenticacao from './pages/Autenticacao'
 
 export default function Routes() {
     return (
-        <BrowserRouter basename="/topicos-frontend">
+        <BrowserRouter basename="/frontend-battleclass">
             <Route path='/' exact component={Login} />
             <Route path='/register' component={Register} />
             <Route path='/main' exact component={MainStudent} />
