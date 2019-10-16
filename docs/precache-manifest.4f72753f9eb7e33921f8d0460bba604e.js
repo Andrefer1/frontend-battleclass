9332cd34b5a01c952b977b0901f4501a63b84952
JvSe <joaovitorsoares12@gmail.com>
2019-10-16 17:53:52 -0300

    2.1.0: Adicionando a pasta docs

---
+++ docs/precache-manifest.4f72753f9eb7e33921f8d0460bba604e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895eda924e0e752affb6bacf923751e9",
+    "url": "/topicos-frontend/index.html"
+  },
+  {
+    "revision": "bb324c04babfb1d452b4",
+    "url": "/topicos-frontend/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3c49f90033f729d6f175",
+    "url": "/topicos-frontend/static/css/main.a8fc3ee8.chunk.css"
+  },
+  {
+    "revision": "bb324c04babfb1d452b4",
+    "url": "/topicos-frontend/static/js/2.34e84654.chunk.js"
+  },
+  {
+    "revision": "3c49f90033f729d6f175",
+    "url": "/topicos-frontend/static/js/main.3ed4f393.chunk.js"
+  },
+  {
+    "revision": "7862d614a5f61060c81a",
+    "url": "/topicos-frontend/static/js/runtime-main.58615003.js"
+  },
+  {
+    "revision": "c8a83d0788097e656bc0129ba6021912",
+    "url": "/topicos-frontend/static/media/github.c8a83d07.png"
+  },
+  {
+    "revision": "7a21cbe90fd31540fb0384792fbc4a86",
+    "url": "/topicos-frontend/static/media/user-profile.7a21cbe9.svg"
+  },
+  {
+    "revision": "dc83660133c322207535335617ef36e6",
+    "url": "/topicos-frontend/static/media/user2.dc836601.png"
+  }
+]);
