f37699f1593b71e3955940c8d588d207f6f4c479
André <andrefer483@gmail.com>
2019-10-20 19:46:14 -0300

    AddActivity screen updated

--- src/pages/Professor/AddActivity.js
+++ src/pages/Professor/AddActivity.js
@@ -7,36 +7,140 @@ import userProfile from '../../assets/user-profile.svg';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-import {Editor, EditorState} from 'draft-js';
-
 import './AddActivity.css';
 
 export default function Activity() {
     const [showMembers, setShowMembers] = useState('')
 
+    var qtd_alternativas = 0
+
     function dropdown() {
         setShowMembers('true')
+
     }
-    ClassicEditor
-        .create( document.querySelector( '#editor' ), {
-            removePlugins: [ 'Bold', 'Link' ],
-            toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
-            heading: {
-                options: [
-                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-                    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-                    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
-                ]
-            }
-        } )
-        .catch( error => {
-            console.log( error );
-        } );
 
-        const [editorState, setEditorState] = React.useState(
-            EditorState.createEmpty()
-          );
+    function create_CKEditor(){
+       let radioHTML = "<input type='radio' name='radioButton' className='input-radio' />"
+        let get_div_radio = document.querySelector('.radio-alternative')
+        /*/*let create_radio = document.createElement('radio')*/
+        /*/*get_div_radio.appendChild(radioHTML)*/
+        /*get_div_radio.innerHTML = radioHTML*/
 
+
+        /*RADIO*/
+        let get_div_input_br = document.querySelector('.radio-alternative')
+        let element_br = document.createElement('br')
+        get_div_input_br.appendChild(element_br)
+
+        let theInput = document.createElement("input");
+        theInput.setAttribute('type',"radio");
+        theInput.setAttribute('name','radioButton');
+        theInput.setAttribute('className','input-radio');
+        get_div_radio.appendChild(theInput)
+
+        let get_div_input_br2 = document.querySelector('.radio-alternative')
+        let element_br2 = document.createElement('br')
+        get_div_input_br2.appendChild(element_br2)
+
+
+        /*EDITOR*/
+        let get_div_editor_br = document.querySelector('.editor-alternative')
+        let create_br = document.createElement('br')
+        get_div_editor_br.appendChild(create_br)
+        /*get_div_radio.appendChild(create_br)*/
+
+        /*let CKEditorHTML = "<CKEditor id='Editor-teste' editor={ ClassicEditor } onInit={ editor => { // You can store the 'editor' and use when it is needed. console.log( 'Editor is ready to use!', editor ); } } onChange={ ( event, editor ) => { const data = editor.getData(); console.log( { event, editor, data } ); } } onBlur={ ( event, editor ) => { console.log( 'Blur.', editor ); } } onFocus={ ( event, editor ) => { console.log( 'Focus.', editor ); } } />"
+        */
+        ClassicEditor
+            .create( document.querySelector( '#editor' ), {
+                /*removePlugins: [ 'Bold', 'Link' ],*/
+                toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+                heading: {
+                    options: [
+                        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+                    ]
+                }
+            } )
+            .catch( error => {
+                console.log( error );
+            } );
+        let CKEditorHTML = `<CKEditor'
+                editor={ ${ClassicEditor} }
+                
+                onInit={ editor => {
+                    // You can store the "editor" and use when it is needed.
+                    console.log( 'Editor is ready to use!', editor );
+                } }
+                onChange={ ( event, editor ) => {
+                    const data = editor.getData();
+                    console.log( { event, editor, data } );
+                } }
+                onBlur={ ( event, editor ) => {
+                    console.log( 'Blur.', editor );
+                } }
+                onFocus={ ( event, editor ) => {
+                    console.log( 'Focus.', editor );
+                } }
+            />`
+
+
+        let get_div_editor = document.querySelector('.editor-alternative')
+        /*let get_CKEditor = document.getElementById('Editor-teste')
+        let create_editor = document.createElement(get_CKEditor)
+        get_div_editor.appendChild(create_editor)*/
+        get_div_editor.innerHTML = CKEditorHTML
+
+        let get_div_editor_br2 = document.querySelector('.editor-alternative')
+        let create_br2= document.createElement('br')
+        get_div_editor_br2.appendChild(create_br2)
+
+     /*alternativas()*/
+    }
+/*
+    function alternativas() {
+        this.qtd_alternativas += 1
+
+        if (this.qtd_alternativas >= 2){
+            /*let elementButtonAdd = document.querySelector('.add').disabled = false*/
+            /*document.getElementsById("teste").disabled = false;
+        }
+
+        else {
+            document.getElementsById("teste").disabled = true
+
+        }
+    }*/
+
+
+    
+
+/*
+    function check_github() {
+        get_input = document.querySelector('input[name=user]')
+        
+    }
+
+    let btnElement = document.querySelector('button')
+    btnElement.onclick = check_github
+
+    function create_list(dev){
+        let ul = document.querySelector('ul')
+            
+        for (dev of dev_list) {
+            console.log(dev.data)
+            let li = document.createElement('li')
+            ul.appendChild(li)
+            li.append(dev.data.name)
+        }
+    }*/
+    
+
+    
+
+
+    
 
 
     return (
@@ -140,28 +244,57 @@ export default function Activity() {
                 </div>
             </div>
 
-            <div className='cards-questions'>
+            <div className='content'>
+                <div className='input-title'>
+                    <label><strong>Título da atividade</strong></label>
+                    <input type='text' className="form-control" placeholder='Adicione um título'  />
+                </div>
+                <div className='cards-questions'>
+                    {/*<strong>Conteúdo</strong>*/}
+                    <div className='editor'>
+                        {/*onclick={get_CKditor()}*/}
+                        <CKEditor id='Editor-teste'
+                            editor={ ClassicEditor }
+      
+                            onChange={ ( event, editor ) => {
+                                const data = editor.getData();
+                                console.log( { event, editor, data } );
+                            } }
+                            onBlur={ ( event, editor ) => {
+                                console.log( 'Blur.', editor );
+                            } }
+                            onFocus={ ( event, editor ) => {
+                                console.log( 'Focus.', editor );
+                            } }
+                        />
+                    </div>
 
-                <CKEditor
-                    editor={ ClassicEditor }
-                    
-                    onInit={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />
+                    <div className='alternative'>
+                        <strong>Alternativas</strong><br />
+                        <div className='radio-editor'>
+                            <div className='radio-alternative'>
 
-               
+                            </div>
+                            <div className='editor-alternative'>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div className='buttons'>
+                        <div>
+                            <button type="button" className="btn btn-outline-primary add-alternative" onClick={create_CKEditor}>+ Alternativa</button>
+                        </div>
+                        <div>
+                            <button className='btn btn-primary add' id='teste'>Adicionar</button>
+                        </div>
+                    </div>
+
+
+
+                </div>
             </div>
         </div>
     );
